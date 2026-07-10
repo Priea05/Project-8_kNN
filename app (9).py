73599792885be5dbce84127927adf32125b1451c
Priea05 <srivastavapriya3011@gmail.com>
2026-07-10 01:22:43 -0700

    Update app (9).py

--- app (9).py
+++ app (9).py
@@ -8,13 +8,12 @@ Original file is located at
 """
 
 import streamlit as st
-import joblib
 import numpy as np
 import os
 import joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "knn_model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "knn_model (2).pkl")
 
 model = joblib.load(MODEL_PATH)
 
